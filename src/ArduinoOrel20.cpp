6258260ab4f6c299d00d2e1de0366851fd074509
Alexander Entinger <cto@lxrobotics.com>
2020-02-12 11:37:29 +0100

    Stripping out invocation of libcanard for now since the API of the library libcanard has changed drastically

--- src/ArduinoOrel20.cpp
+++ src/ArduinoOrel20.cpp
@@ -16,28 +16,9 @@
 
 ArduinoOrel20::ArduinoOrel20()
 {
-  canardInit(&_canard_inst,
-             reinterpret_cast<void *>(&_libcanard_memory_pool),
-             LIBCANARD_MEMORY_POOL_SIZE_BYTES,
-             ArduinoOrel20::onTransferReception,
-             ArduinoOrel20::shouldAcceptTransfer,
-             reinterpret_cast<void *>(this));
+  /* TODO */
 }
 
 /**************************************************************************************
  * PUBLIC MEMBER FUNCTIONS
  **************************************************************************************/
-
-void ArduinoOrel20::onTransferReception(CanardInstance * ins, CanardRxTransfer * transfer)
-{
-  ArduinoOrel20 * this_ptr = reinterpret_cast<ArduinoOrel20 *>(ins->user_reference);
-
-  /* TODO */
-}
-
-bool ArduinoOrel20::shouldAcceptTransfer(const CanardInstance * ins, uint64_t * out_data_type_signature, uint16_t data_type_id, CanardTransferType transfer_type, uint8_t source_node_id)
-{
-  ArduinoOrel20 const * this_ptr = reinterpret_cast<ArduinoOrel20 const *>(ins->user_reference);
-
-  /* TODO */
-}
--- src/ArduinoOrel20.h
+++ src/ArduinoOrel20.h
@@ -14,7 +14,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#include <canard.h>
+#include <libcanard/canard.h>
 
 /**************************************************************************************
  * CLASS DECLARATION
@@ -27,16 +27,7 @@ public:
   ArduinoOrel20();
 
 
-  static void onTransferReception(CanardInstance* ins, CanardRxTransfer* transfer);
-  static bool shouldAcceptTransfer(const CanardInstance* ins, uint64_t* out_data_type_signature, uint16_t data_type_id, CanardTransferType transfer_type, uint8_t source_node_id);
-
-
 private:
-
-  CanardInstance _canard_inst;
-
-  static size_t const LIBCANARD_MEMORY_POOL_SIZE_BYTES = 1024;
-  uint8_t _libcanard_memory_pool[LIBCANARD_MEMORY_POOL_SIZE_BYTES];
 
 };
 
