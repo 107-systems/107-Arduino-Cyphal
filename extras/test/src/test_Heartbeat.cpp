64e2ff102f1cc93bc363beda13d845fa8796df28
Alexander Entinger <cto@lxrobotics.com>
2020-06-05 12:06:53 +0200

    First draft of 107-Arduino-UAVCAN receive API complete

--- extras/test/src/test_Heartbeat.cpp
+++ extras/test/src/test_Heartbeat.cpp
@@ -24,6 +24,13 @@ TEST_CASE("")
   util::Transfer transfer_util;
   
   ArduinoUAVCAN uavcan(13, time_util.micros(), transfer_util.onTransferReceived());
+
+  uavcan.subscribeMessage(32085, 8);
+
+  uint8_t const data[] = {0x39, 0x05, 0x00, 0x00, 0x00, 0x00, 0x00, 0xE1};
+  uavcan.onCanFrameReceived(0x107D553B, data, 8);
   
   REQUIRE(transfer_util.received());
+  REQUIRE(transfer_util().port_id == 32085);
+  REQUIRE(transfer_util().remote_node_id == 59);
 }
