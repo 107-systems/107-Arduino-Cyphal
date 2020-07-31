198ea289d5ce101db3b8872a5705a1a02dea3702
Alexander Entinger <cto@lxrobotics.com>
2020-07-31 04:55:59 +0200

    Rewriting license, copyright, author information

--- extras/test/include/test/util/Const.h
+++ extras/test/include/test/util/Const.h
@@ -1,6 +1,8 @@
 /**
- * @brief   Test code for testing 107-Arduino-UAVCAN.
- * @license LGPL 3.0
+ * This software is distributed under the terms of the MIT License.
+ * Copyright (c) 2020 LXRobotics.
+ * Author: Alexander Entinger <alexander.entinger@lxrobotics.com>
+ * Contributors: https://github.com/107-systems/107-Arduino-UAVCAN/graphs/contributors.
  */
 
 #ifndef TEST_UTIL_CONST_H_
