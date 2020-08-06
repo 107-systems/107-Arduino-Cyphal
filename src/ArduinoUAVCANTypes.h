59db1665ba7e1adbfcb26a8813761acddfd7f94c
Alexander Entinger <consulting@lxrobotics.com>
2020-08-06 16:29:29 +0200

    Including all types within ArduinoUAVCAN so as to save the user to include the individual type they want to use (#30)

---
+++ src/ArduinoUAVCANTypes.h
@@ -0,0 +1,18 @@
+/**
+ * This software is distributed under the terms of the MIT License.
+ * Copyright (c) 2020 LXRobotics.
+ * Author: Alexander Entinger <alexander.entinger@lxrobotics.com>
+ * Contributors: https://github.com/107-systems/107-Arduino-UAVCAN/graphs/contributors.
+ */
+
+#ifndef ARDUINO_UAVCAN_TYPES_H_
+#define ARDUINO_UAVCAN_TYPES_H_
+
+/**************************************************************************************
+ * INCLUDE
+ **************************************************************************************/
+
+#include "types/uavcan/node/Heartbeat.1.0.h"
+#include "types/uavcan/node/ExecuteCommand.1.0.h"
+
+#endif /* ARDUINO_UAVCAN_TYPES_H_ */
