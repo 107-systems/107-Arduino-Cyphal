cde3865c6829f766ba6297a04e92843a668baf3a
Alexander Entinger <cto@lxrobotics.com>
2020-12-31 04:57:00 +0100

    All copyright retains with LXRobotics, otherwise we would create a nightmare for potential business adopters if everyone would claim copyright for a couple of files.

--- src/types/uavcan/primitive/scalar/Bit.1.0.hpp
+++ src/types/uavcan/primitive/scalar/Bit.1.0.hpp
@@ -1,7 +1,7 @@
 /**
  * This software is distributed under the terms of the MIT License.
- * Copyright (c) 2020 generationmake.
- * Author: Bernhard Mayer
+ * Copyright (c) 2020 LXRobotics.
+ * Author: Bernhard Mayer <bernard@generationmake.de>, Alexander Entinger <alexander.entinger@lxrobotics.com>
  * Contributors: https://github.com/107-systems/107-Arduino-UAVCAN/graphs/contributors.
  */
 
