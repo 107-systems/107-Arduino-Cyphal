bece8710f510415281fd4449bdfc1e875af52a60
generationmake <37663074+generationmake@users.noreply.github.com>
2020-12-31 16:25:41 +0100

    Update examples/UAVCAN-Blink/UAVCAN-Blink.ino
    corrected pyuavcan command
    
    Co-authored-by: Pavel Kirienko <pavel.kirienko@gmail.com>

--- examples/UAVCAN-Blink/UAVCAN-Blink.ino
+++ examples/UAVCAN-Blink/UAVCAN-Blink.ino
@@ -3,7 +3,7 @@
  * It also shows periodic transmission of a UAVCAN heartbeat message via CAN.
  *
  * switch built in LED on with
- *   pyuavcan pub 1620.uavcan.primitive.scalar.Bit.1.0 {'value: true'} --tr='CAN(can.media.socketcan.SocketCANMedia("can0",8),59)'
+ *   pyuavcan pub 1620.uavcan.primitive.scalar.Bit.1.0 'value: true' --tr='CAN(can.media.socketcan.SocketCANMedia("can0",8),59)'
  *
  * switch built in LED off with
  *   pyuavcan pub 1620.uavcan.primitive.scalar.Bit.1.0 {'value: false'} --tr='CAN(can.media.socketcan.SocketCANMedia("can0",8),59)'
