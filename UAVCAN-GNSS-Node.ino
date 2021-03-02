7cd28c1d540e1c445b7206746f02cd6c1b31dca2
Alexander Entinger <consulting@lxrobotics.com>
2021-03-02 09:30:19 +0100

    Fixing CI compilation issue due 107-Arduino-MCP2515:v1.3.0. (#11)

--- UAVCAN-GNSS-Node.ino
+++ UAVCAN-GNSS-Node.ino
@@ -142,7 +142,7 @@ void setup()
 
   /* Configure MCP2515 */
   mcp2515.begin();
-  mcp2515.setBitRate(CanBitRate::BR_250kBPS);
+  mcp2515.setBitRate(CanBitRate::BR_250kBPS_16MHZ);
   mcp2515.setListenOnlyMode();
 }
 
