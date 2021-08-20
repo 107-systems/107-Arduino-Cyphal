4e1ad5772cd01123956d0358804aedae4f2a60aa
Alexander Entinger <cto@lxrobotics.com>
2021-08-20 12:58:57 +0200

    Fixing example file.

--- examples/UAVCAN-ToF-Distance-Sensor-Node/UAVCAN-ToF-Distance-Sensor-Node.ino
+++ examples/UAVCAN-ToF-Distance-Sensor-Node/UAVCAN-ToF-Distance-Sensor-Node.ino
@@ -36,7 +36,7 @@
 
 typedef struct
 {
-  uavcan::node::Heartbeat_1_0::Mode mode;
+  uavcan::node::Heartbeat_1_0<>::Mode mode;
 } UavcanNodeData;
 
 #warning "Run 'TMF8801-FactoryCalib' once in order to obtain sensor calibration data for node configuration 'calib_data'"
@@ -46,6 +46,7 @@ typedef struct
   uint32_t heartbeat_period_ms;
   TMF8801::CalibData calib_data;
   TMF8801::AlgoState algo_state;
+  uint8_t measurement_period_ms;
 } UavcanNodeConfiguration;
 
 /**************************************************************************************
@@ -57,13 +58,14 @@ static int            const MKRCAN_MCP2515_INT_PIN   = 7;
 static uint8_t        const UAVCAN_NODE_ID           = 14;
 static UavcanNodeData const UAVCAN_NODE_INITIAL_DATA =
 {
-  uavcan::node::Heartbeat_1_0::Mode::INITIALIZATION,
+  uavcan::node::Heartbeat_1_0<>::Mode::INITIALIZATION,
 };
 static UavcanNodeConfiguration const UAVCAN_NODE_INITIAL_CONFIGURATION =
 {
   1000,
   {0x31, 0x9E, 0x0, 0xB6, 0x9, 0xE0, 0xFB, 0xF7, 0xF8, 0xF1, 0xE3, 0xC7, 0x7, 0xFC},
-  {0xB1, 0xA9, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
+  {0xB1, 0xA9, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+  100
 };
 
 /**************************************************************************************
@@ -85,15 +87,15 @@ bool transmit(CanardFrame const &);
 
 namespace node
 {
-uavcan::node::Heartbeat_1_0::Mode handle_INITIALIZATION();
-uavcan::node::Heartbeat_1_0::Mode handle_OPERATIONAL();
-uavcan::node::Heartbeat_1_0::Mode handle_MAINTENANCE();
-uavcan::node::Heartbeat_1_0::Mode handle_SOFTWARE_UPDATE();
+uavcan::node::Heartbeat_1_0<>::Mode handle_INITIALIZATION();
+uavcan::node::Heartbeat_1_0<>::Mode handle_OPERATIONAL();
+uavcan::node::Heartbeat_1_0<>::Mode handle_MAINTENANCE();
+uavcan::node::Heartbeat_1_0<>::Mode handle_SOFTWARE_UPDATE();
 }
 
 namespace heartbeat
 {
-void publish(ArduinoUAVCAN &, uint32_t const, uavcan::node::Heartbeat_1_0::Mode const);
+void publish(ArduinoUAVCAN &, uint32_t const, uavcan::node::Heartbeat_1_0<>::Mode const);
 }
 
 /**************************************************************************************
@@ -151,7 +153,7 @@ void setup()
 
   /* Configure TMF8801
    */
-  if (!tmf8801.begin()) {
+  if (!tmf8801.begin(node_config.measurement_period_ms)) {
     DBG_ERROR("ArduinoTMF8801::begin(...) failed, error code %d", (int)tmf8801.error());
     for(;;) { }
   }
@@ -175,14 +177,14 @@ void loop()
 
   /* Handle state transitions and state specific action.
    */
-  uavcan::node::Heartbeat_1_0::Mode next_mode = node_data.mode;
+  uavcan::node::Heartbeat_1_0<>::Mode next_mode = node_data.mode;
 
   switch(node_data.mode)
   {
-  case uavcan::node::Heartbeat_1_0::Mode::INITIALIZATION:  next_mode = node::handle_INITIALIZATION();  break;
-  case uavcan::node::Heartbeat_1_0::Mode::OPERATIONAL:     next_mode = node::handle_OPERATIONAL();     break;
-  case uavcan::node::Heartbeat_1_0::Mode::MAINTENANCE:     next_mode = node::handle_MAINTENANCE();     break;
-  case uavcan::node::Heartbeat_1_0::Mode::SOFTWARE_UPDATE: next_mode = node::handle_SOFTWARE_UPDATE(); break;
+  case uavcan::node::Heartbeat_1_0<>::Mode::INITIALIZATION:  next_mode = node::handle_INITIALIZATION();  break;
+  case uavcan::node::Heartbeat_1_0<>::Mode::OPERATIONAL:     next_mode = node::handle_OPERATIONAL();     break;
+  case uavcan::node::Heartbeat_1_0<>::Mode::MAINTENANCE:     next_mode = node::handle_MAINTENANCE();     break;
+  case uavcan::node::Heartbeat_1_0<>::Mode::SOFTWARE_UPDATE: next_mode = node::handle_SOFTWARE_UPDATE(); break;
   }
 
   node_data.mode = next_mode;
@@ -259,32 +261,32 @@ bool transmit(CanardFrame const & frame) {
 namespace node
 {
 
-uavcan::node::Heartbeat_1_0::Mode handle_INITIALIZATION()
+uavcan::node::Heartbeat_1_0<>::Mode handle_INITIALIZATION()
 {
   DBG_VERBOSE("INITIALIZATION");
 
-  return uavcan::node::Heartbeat_1_0::Mode::OPERATIONAL;
+  return uavcan::node::Heartbeat_1_0<>::Mode::OPERATIONAL;
 }
 
-uavcan::node::Heartbeat_1_0::Mode handle_OPERATIONAL()
+uavcan::node::Heartbeat_1_0<>::Mode handle_OPERATIONAL()
 {
   DBG_VERBOSE("OPERATIONAL");
 
-  return uavcan::node::Heartbeat_1_0::Mode::OPERATIONAL;
+  return uavcan::node::Heartbeat_1_0<>::Mode::OPERATIONAL;
 }
 
-uavcan::node::Heartbeat_1_0::Mode handle_MAINTENANCE()
+uavcan::node::Heartbeat_1_0<>::Mode handle_MAINTENANCE()
 {
   DBG_VERBOSE("MAINTENANCE");
 
-  return uavcan::node::Heartbeat_1_0::Mode::INITIALIZATION;
+  return uavcan::node::Heartbeat_1_0<>::Mode::INITIALIZATION;
 }
 
-uavcan::node::Heartbeat_1_0::Mode handle_SOFTWARE_UPDATE()
+uavcan::node::Heartbeat_1_0<>::Mode handle_SOFTWARE_UPDATE()
 {
   DBG_VERBOSE("SOFTWARE_UPDATE");
 
-  return uavcan::node::Heartbeat_1_0::Mode::INITIALIZATION;
+  return uavcan::node::Heartbeat_1_0<>::Mode::INITIALIZATION;
 }
 
 } /* node */
@@ -292,12 +294,12 @@ uavcan::node::Heartbeat_1_0::Mode handle_SOFTWARE_UPDATE()
 namespace heartbeat
 {
 
-void publish(ArduinoUAVCAN & u, uint32_t const uptime, uavcan::node::Heartbeat_1_0::Mode const mode)
+void publish(ArduinoUAVCAN & u, uint32_t const uptime, uavcan::node::Heartbeat_1_0<>::Mode const mode)
 {
-  uavcan::node::Heartbeat_1_0 hb;
+  uavcan::node::Heartbeat_1_0<> hb;
 
   hb.data.uptime = uptime;
-  hb = uavcan::node::Heartbeat_1_0::Health::NOMINAL;
+  hb = uavcan::node::Heartbeat_1_0<>::Health::NOMINAL;
   hb = mode;
   hb.data.vendor_specific_status_code = 0;
 
