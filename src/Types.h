b9134195259fefc8e9492204bbbf763b783e8295
Alexander Entinger <consulting@lxrobotics.com>
2023-01-05 07:01:36 +0100

    Move definition of CyphalHeap over to Types.h

--- src/Types.h
+++ src/Types.h
@@ -12,9 +12,11 @@
  * INCLUDE
  **************************************************************************************/
 
+#include <array>
 #include <functional>
 
 #include "libcanard/canard.h"
+#include "libo1heap/o1heap.h"
 
 /**************************************************************************************
  * TYPEDEF
@@ -22,5 +24,8 @@
 
 typedef std::function<CanardMicrosecond(void)> CyphalMicrosFunc;
 typedef std::function<bool(CanardFrame const &)> CyphalCanFrameTxFunc;
+
+template <size_t SIZE>
+struct alignas(O1HEAP_ALIGNMENT) CyphalHeap final : public std::array<uint8_t, SIZE> {};
 
 #endif /* INC_107_ARDUINO_CYPHAL_CONST_H */
