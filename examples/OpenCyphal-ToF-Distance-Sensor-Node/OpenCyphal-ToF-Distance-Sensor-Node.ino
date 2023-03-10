dd2e14df172edcae04e1fdf8c34a210451b21980
Alexander Entinger <consulting@lxrobotics.com>
2023-03-10 16:07:34 +0100

    Drop std::string_view in favour of const char array.

--- examples/OpenCyphal-ToF-Distance-Sensor-Node/OpenCyphal-ToF-Distance-Sensor-Node.ino
+++ examples/OpenCyphal-ToF-Distance-Sensor-Node/OpenCyphal-ToF-Distance-Sensor-Node.ino
@@ -170,11 +170,11 @@ RegisterList reg_list(node_hdl, micros);
 const auto reg_rw_cyphal_node_id = reg_list.registry.expose
   ("cyphal.node.id", {}, node_id);
 const auto reg_ro_cyphal_node_description = reg_list.registry.route
-  ("cyphal.node.description", {true}, []() { return std::string_view("OpenCyphal-ToF-Distance-Sensor-Node"); });
+  ("cyphal.node.description", {true}, []() { return "OpenCyphal-ToF-Distance-Sensor-Node"; });
 const auto reg_rw_cyphal_pub_distance_id = reg_list.registry.expose
   ("cyphal.pub.distance.id", {true}, distance_data_port_id);
 const auto reg_ro_cyphal_pub_distance_type = reg_list.registry.route
-  ("cyphal.pub.distance.type", {true}, []() { return std::string_view("cyphal.primitive.scalar.Real32.1.0"); });
+  ("cyphal.pub.distance.type", {true}, []() { return "cyphal.primitive.scalar.Real32.1.0"; });
 
 /* NODE INFO **************************************************************************/
 
