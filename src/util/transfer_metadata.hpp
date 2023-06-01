b355e85abd471e88af4f02beb01a0d80a6080be7
Rebecca <rebecca@ross-robotics.co.uk>
2023-06-01 16:17:37 +0100

    change to uint16_t

--- src/util/transfer_metadata.hpp
+++ src/util/transfer_metadata.hpp
@@ -8,19 +8,9 @@
 #ifndef ARDUINO_CYPHAL_TRANSFER_METADATA_HPP_
 #define ARDUINO_CYPHAL_TRANSFER_METADATA_HPP_
 
-#if defined __has_include
-#  if __has_include (<libcanard/canard.h>)
-  using CyphalNodeID = CanardNodeID;
-#  elif __has_include (<lipudpard/udpard.h>)
-  using CyphalNodeID = UdpardNodeID;
-#  endif
-#else
-# error "We only support CAN or UDP as Cyphal transport layer"
-#endif
-
 struct TransferMetadata final
 {
-  CyphalNodeID node_id;
+  uint16_t node_id;
   // More stuff may appear here in the future!
 };
 
